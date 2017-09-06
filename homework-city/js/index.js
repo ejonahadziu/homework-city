cfec50147debfd3d857d9b72b976a35977e471c7
ejonahadziu <30577990+ejonahadziu@users.noreply.github.com>
2017-09-06 16:56:15 -0400

    draft3

--- homework-city/js/index.js
+++ homework-city/js/index.js
@@ -1,13 +1,15 @@
  // When the user enters a city name on the input field 
  //And clicks on the update button
  //Then the user should see the background of the page according to the selection made (i.e if user selected NYC/New York/New York City, the background image should be nyc.jpg)
+ // When the user enters a city name will all lower case letter or extra spaces on the input field 
+ //And clicks on the update button
+ //Then the user should see the background of the page according to the selection made
  //When the background gets updated 
  //Then the input field should be reset back to the orignial state (i.e "enter a city name" placeholder)
 
 $(document).ready(function() {
 	
 	$('#submit-btn').click(Submit);
-
 
 	function Submit () {
 		var city = $('#city-type').val();
@@ -22,12 +24,11 @@ $(document).ready(function() {
 		var imageCity;
 		city = city.toUpperCase();
 		city = city.trim();
-		$("body")[0].className="";
+		$('body')[0].className = '';
 
 		if (city === 'NYC' || city === 'NEW YORK' || city === 'NEW YORK CITY') {
-			imageCity = 'nyc';
+			imageCity = 'nyc'; // upper case letters used for all city options because of the toUpperCase validation
 		} else if (city === 'SAN FRANCISCO' || city ==='BAY AREA' || city ==='SF') {
-			// upper case letters used for all city options because of the toUpperCase validation
 			imageCity = 'sf';
 		} else if (city === 'LOS ANGELES' || city ==='LA' || city ==='LAX') {
 			imageCity = 'la';
@@ -37,7 +38,7 @@ $(document).ready(function() {
 			imageCity = 'sydney';
 		}
 		$('body').addClass(imageCity);
-		
+		$('#city-type').val('');
 	}
 
 	// Remove extra spaces or new lines that users or the browser might add before or after their input
